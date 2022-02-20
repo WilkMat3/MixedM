fa2dd6a0ad12bd3ccb904027e7e5e94efe8aeb0e
guled egal <guledegal91@gmail.com>
2022-02-20 11:24:58 +0000

    added different proverbs, removed return num,

--- index.js
+++ index.js
@@ -10,22 +10,27 @@ console.log(`${input}`)
 const theMessage = param => console.log(param);
 
 // The Chinese proverb function 
+let first = "Chinese proverb: A crisis is an opportunity riding the dangerous wind."
+let second = "Chinese proverb: It's better to be without a book than to believe a book entirely."
+let third = "Chinese proverb: A little impatience will spoil great plans."
+let fourth = "A journey of a thousand miles begins with a single step."
+let fifth = "Be not afraid of growing slowly, be afraid only of standing still."
+
 
 let gen = () =>{
     num = Math.floor(Math.random() * 5);
     switch (num) {
-        case 0: console.log("Chinese proverb: A crisis is an opportunity riding the dangerous wind.");
+        case 0: console.log(first);
         break;
-        case 1: console.log("Chinese proverb: It's better to be without a book than to believe a book entirely.");
+        case 1: console.log(second);
         break;
-        case 2: console.log("Chinese proverb: A little impatience will spoil great plans.");
+        case 2: console.log(third);
         break;
-        case 3: console.log("Chinese proverb: A journey of a thousand miles begins with a single step.");
+        case 3: console.log(fourth);
         break;
-        case 4: console.log("Chinese proverb: Behave toward everyone as if receiving a guest.");
+        case 4: console.log(fifth);
         break;
     }
-    return(num)
   };
 
 // This is the switch statement that will check the user input to the response.
